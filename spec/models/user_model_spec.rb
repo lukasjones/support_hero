586685d812fe1feb3a1ba8c80d29a1370c90551c
Lukas Jones <joneslukas21@gmail.com>
2015-07-23 16:22:02 -0700

    added user model tests

---
+++ spec/models/user_model_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper.rb'
+
+describe "user model" do
+
+	it "should save a user if all the fields are filled in correctly" do 
+
+		user = User.new(name: "John Doe", email: "john@doe.com", password: "johndoe123")
+		expect(user.save).to eq(true)
+	end
+
+	it "should not save a user if the name is missing" do
+		user = User.new(email: "john@doe.com", password: "johndoe123")
+
+		expect(user.save).to eq(false)
+	end
+
+	it "should not save a user if the email is missing" do
+		user = User.new(name: "john", password: "johndoe123")
+
+		expect(user.save).to eq(false)
+	end
+
+
+
+
+	
+end
