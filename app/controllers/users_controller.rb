834cb79de061d1922bb83dabafeede29e564f76c
Lukas Jones <joneslukas21@gmail.com>
2015-07-25 21:37:02 -0600

    added find user form that redirects to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	def index
+		@errors = []
 		@todays_hero = DayAssignment.where(date: DateTime.now.to_date)[0].user
 	end
 
@@ -7,7 +8,17 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def find
+		user = User.where(name: params[:find][:name])[0]
 
+		if user
+			redirect_to user
+		else
+			@todays_hero = DayAssignment.where(date: DateTime.now.to_date)[0].user
+			@error = ["That user doesn't exist"]
+			render "index"
+		end
+	end
 
 
 end
