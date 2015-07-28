ea33b1528000dcb4f4c0d33ee6764438e006e1b6
Lukas Jones <joneslukas21@gmail.com>
2015-07-28 14:40:59 -0600

    removed out dated current month helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,5 @@
 module UsersHelper
 
-	# def current_month
-	# 	current_month_array = []
-	# 	month = DateTime.now.month
-	# 	year = DateTime.now.year
-	# 	current_day = "1-#{month}-#{year}".to_date
-
-	# 	until current_day.month != month do
-	# 		current_month_array.push(current_day)
-	# 		current_day = current_day.tomorrow.to_date
-	# 	end
-
-	# 	current_month_array
-	# end
-
 	def get_header_phrase
 		day = DateTime.now.to_date.strftime("%A")
 		if day === "Saturday" || day === "Sunday"
