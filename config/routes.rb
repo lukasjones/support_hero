0930e0a05a713a0fa0feebb4cb9d65433c47f936
Lukas Jones <joneslukas21@gmail.com>
2015-07-30 17:30:14 -0600

    minor name and description changes, added update route for user no can do day

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'users#index'
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :update]
 
   post 'find' => 'users#find'
 
