1df8f622b05331d8cad5ccbbc72eeb3f2298f399
Lukas Jones <joneslukas21@gmail.com>
2015-07-30 20:13:21 -0600

    added js to handle hover and display no can do form. Put calendar into shared view

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	$form = $(".user_box").html();
+
+	$("html").on("mouseover", "td.selected", function(e){
+		$(this).find("#user_no_can_do_day").val($(this).attr("data-date"))
+		$(this).find("a").hide();
+		$(this).find(".cant-do").show();
+	})
+
+	$("html").on("mouseleave", "td.selected", function(){
+		$(this).find("a").show();
+		$(this).find(".cant-do").hide();
+	})
+
+
+
+})
