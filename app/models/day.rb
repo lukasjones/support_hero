4b54f20cd52df1f4abcce804cdc002f544e7919a
Lukas Jones <joneslukas21@gmail.com>
2015-08-01 21:22:46 -0700

    added day model to handle assigned and non assigned days

---
+++ app/models/day.rb
@@ -0,0 +1,27 @@
+class Day
+	include ActiveModel::Model
+
+	attr_reader :date, :week_day_name, :num, :scheduled_user_name, :scheduled_user_id
+
+	attr_accessor :scheduled_user
+
+
+	def initialize(attributes={})
+		@date                = attributes[:date] || nil
+		@scheduled_user      = attributes[:user] || nil 
+		@scheduled_user_name = @scheduled_user ? @scheduled_user.name : nil
+		@scheduled_user_id   = @scheduled_user ? @scheduled_user.id : nil
+		@week_day_name       = @date ? @date.strftime("%A") : nil
+		@num                 = @date ? @date.day : nil
+	end
+
+	def update_user(user)
+		@scheduled_user      = user
+		@scheduled_user_name = user.name
+		@scheduled_user_id   = user.id
+	end
+
+	
+
+end
+
