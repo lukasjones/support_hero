a6fd67d31cda7241a5e9490c17f93559fc669aed
Lukas Jones <joneslukas21@gmail.com>
2015-08-01 21:16:46 -0700

    added new swap method and fixed unnecessary space bug that made holiday not register

--- app/models/day_assignment.rb
+++ app/models/day_assignment.rb
@@ -8,6 +8,8 @@ class DayAssignment < ActiveRecord::Base
 	validate :check_if_california_holiday
 
 
+
+
 	def cannot_be_weekend
 		day = self.date.strftime("%A")
 		if day == "Saturday" || day == "Sunday" 
@@ -16,19 +18,29 @@ class DayAssignment < ActiveRecord::Base
 	end
 
 	def check_if_california_holiday
-		holidays = ["01/01", "01/19", "02/16", "03/31", "05/25", "07/04 ", "09/07 ", "11/11", "11/26", "11/27", "12/25"]
+		# "month/day"
+		holidays = ["01/01", "01/19", "02/16", "03/31", "05/25", "07/04", "09/07", "11/11", "11/26", "11/27", "12/25"]
 		month_and_day = self.date.strftime("%m/%d")
-		any_errors = false
-		holidays.each do |holiday|
-			if month_and_day == holiday
-				any_errors = true
-			end
-		end
-
-		if any_errors
+		
+		if holidays.include?(month_and_day)
 			errors.add(:holiday_error, "can't schedule days on California holidays")				
 		end
 
   end
+
+  def swap_user(first_date, second_date)
+		first_user = get_user_from_date(first_date)
+		second_user = get_user_from_date(second_date)
+		DayAssignment.where(date: first_date).update_attributes(user: first_user)
+		DayAssignment.where(date: second_date).update_attributes(user: second_user)
+	end
+
+	private
+
+
+
+	def get_user_from_date(date)
+		DayAssignment.where(date: date)[0].user
+	end
 end
 
