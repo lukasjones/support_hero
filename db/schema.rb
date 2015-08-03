80790444b5073de286129dcfc2bfaf187bec5582
Lukas Jones <joneslukas21@gmail.com>
2015-08-03 16:16:49 -0700

    modified schema on rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729015305) do
+ActiveRecord::Schema.define(version: 20150803195640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20150729015305) do
   create_table "day_assignments", force: :cascade do |t|
     t.integer  "user_id"
     t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "swap_request"
   end
 
   create_table "users", force: :cascade do |t|
