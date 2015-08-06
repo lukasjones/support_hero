31d4d4347d5c1d3f43aa6ef9ae756e0750384449
Lukas Jones <joneslukas21@gmail.com>
2015-08-06 16:38:19 -0700

    added tests for calendar controller

--- spec/controllers/calendar_controller_spec.rb
+++ spec/controllers/calendar_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe CalendarController, type: :controller do
 
+	describe "PUT #change_month" do
+		before(:each) do
+			params = {num: "5"}
+			put :change_month, params
+		end
+
+		it "should respond with status code 200" do
+			expect(response).to be_success
+      expect(response).to have_http_status(200)
+		end
+
+		it "should render a partial" do 
+			should render_template(:partial => 'shared/_calendar') 
+		end
+
+	end
+
 end
