29beb79bb567b098b91588e720b7d74128b67648
Lukas Jones <joneslukas21@gmail.com>
2015-08-06 11:52:03 -0700

    renamed day_assignment model test file and added new tests for day assignments validations

--- spec/models/day_assignment_model_spec.rb
+++ spec/models/day_assignment_model_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 
 describe "Day Assignment Model" do
 
-	let(:user) { User.create(name: "John") }
+	let(:user) { User.create(name: "John", no_can_do_day: '12-12-2015'.to_date) }
 
 
 
@@ -25,6 +25,24 @@ describe "Day Assignment Model" do
 		day_assignment = DayAssignment.create(user: user, date: DateTime.now.to_date)
 		day_assignment2 = DayAssignment.new(user: user, date: DateTime.now.to_date)
 		expect(day_assignment2.save).to eq(false)
+	end
+
+	it "should not save a day on the weekend" do
+		# Saturday August 1st 2015
+		weekend_date = '1-8-2015'.to_date
+		day = DayAssignment.new(user: user, date: weekend_date)
+		expect(day.save).to eq(false)
+	end
+
+	it "should not save a day on a California Holiday" do
+		holidays = ["01/01/2015", "19/01/2015", "16/02/2015", "31/03/2015", "25/05/2015", "04/07/2015", "07/09/2015", "11/11/2015", "26/11/2015", "27/11/2015", "25/12/2015"]
+		day = DayAssignment.new(user: user, date: holidays.sample.to_date)
+		expect(day.save).to eq(false)
+	end
+
+	it "should not save a day on a user's no_can_do_day" do
+		day = DayAssignment.new(user: user, date: '12-12-2015'.to_date)
+		expect(day.save).to eq(false)
 	end
 
 	
