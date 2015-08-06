0a612b629a4a19b63286d4f1e65836d223ffdb09
Lukas Jones <joneslukas21@gmail.com>
2015-08-06 16:08:17 -0700

    added controller tests for find and show actions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,34 +3,79 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
 
-	before(:each) do
-		user = User.create(name: "John", email: "john@john.com", password: "john123")
-		assignment = DayAssignment.create(user: user, date: DateTime.now.to_date)
-	end
 
 	describe "GET #index" do
-		it "should respond with status code 200" do
-			get :index
 
+		before(:each) do
+			user = User.create(name: "John")
+			assignment = DayAssignment.create(user: user, date: Date.today)
+			get :index
+		end
+
+		it "should respond with status code 200" do
 			expect(response).to be_success
       expect(response).to have_http_status(200)
-      
 		end
 
 		it "should render the index template" do
-			get :index
       expect(response).to render_template("index")
 		end
 
 		it "should assign @errors to an empty array" do
-			get :index
 			expect(assigns(:errors)).to eq([])
 		end
 
-		it "should assign todays_hero to user for current day assignment" do
+	end
+
+
+
+	describe "GET #show" do
+
+		before(:each) do
+			user = User.create(name: "Rah")
+			get :show, id: user.id
+		end
+
+		it "should respond with a status code of 200" do
+			expect(response).to be_success
+			expect(response).to have_http_status(200)
+		end
+
+
+
+		it "should set session[:user_id] to id of show page user" do
 			user = User.last
-			get :index
-			expect(assigns(:todays_hero).name).to eq(user.name)
+			expect(session[:user_id]).to eq(user.id)
+		end
+
+	end
+
+
+	describe "POST #find" do
+
+		
+
+		it "should redirect to the show page of user name is in database" do
+
+			user = User.create(name: "Lah")
+			params={}
+			params[:find] = {}
+			params[:find][:name] = "Lah"
+
+			post :find, params
+
+			expect(response).to redirect_to("/users/#{user.id}")
+
+		end
+
+		it "should not redirect and stay on home page if name provided is not in database" do
+			params={}
+			params[:find] = {}
+			params[:find][:name] = "Not a user"
+
+			post :find, params
+
+			expect(response).to redirect_to("/")	
 		end
 
 	end
