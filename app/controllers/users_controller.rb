1ed57122c6f8a0efbf0120a617be28297d6a3baf
Lukas Jones <joneslukas21@gmail.com>
2015-08-06 14:40:18 -0700

    Made users controller more dry with before filter variable setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+
+	before_filter :set_year, only: [:index, :show]
+	before_filter :set_user, only: [:show, :update]
+
 	def index
-		session[:show] = false
 		@errors = []
-		session[:year] = Date.today.year unless session[:year]
+		session[:show] = false
 		session[:selected_user] = "not a user"
 	end
 
 	def show
 		session[:show] = true
-		@user = User.find(params[:id])
 		session[:user_id] = @user.id
-		session[:year] = Date.today.year unless session[:year]
 		session[:selected_user] = @user.name
 	end
 
@@ -28,16 +29,24 @@ class UsersController < ApplicationController
 
 	# make this work for all updates
 	def update
-		user = User.find(params[:id])
-		if user.update_attributes({no_can_do_day: params[:user][:no_can_do_day].to_date})
+		if @user.update_attributes({no_can_do_day: params[:user][:no_can_do_day].to_date})
 			flash[:notice] = "successfully updated"
 		else
-			flash[:errors] = user.errors.values
+			flash[:errors] = @user.errors.values
 		end
 
-		redirect_to user
+		redirect_to @user
 	end
 
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def set_year
+		session[:year] = Date.today.year unless session[:year]
+	end
 
 end
 
