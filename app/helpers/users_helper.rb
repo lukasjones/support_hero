42efcf2029cf9b7d4549658bab5f732186e740bb
Lukas Jones <joneslukas21@gmail.com>
2015-08-08 11:10:29 -0700

    wrote tests for get_header_phrase helper and made it include specialized holiday message

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,14 @@
 module UsersHelper
 
-	def get_header_phrase
-		day = DateTime.now.to_date.strftime("%A")
+	def get_header_phrase(date)
+		day = date.strftime("%A")
 		if day === "Saturday" || day === "Sunday"
 			"It's the weekend. No support today!"
+
+		elsif ["01/01", "01/19", "02/16", "03/31", "05/25", "07/04", "09/07", "11/11", "11/26", "11/27", "12/25"].include?(date.strftime("%m/%d"))
+			"It's a holiday! Go celebrate!"
 		else
-			user = DayAssignment.where(date: DateTime.now.to_date)[0].user
+			user = DayAssignment.where(date: date)[0].user
 			"Today's Support Hero is #{user.name}"
 		end
 	end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,5 +11,26 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "get_header_phrase" do
+
+		it "should give you the weekend phrase if it's the weekend" do
+			weekend_date = '8-8-2015'.to_date
+			phrase = helper.get_header_phrase(weekend_date)
+			expect(phrase).to eq("It's the weekend. No support today!")
+		end
+
+		it "should give you the current_user when it's not a weekend or California Holiday" do
+			user = User.create(name: "John")
+			working_date = "8-10-2015".to_date
+			DayAssignment.create(user: user, date: working_date)
+			phrase = helper.get_header_phrase(working_date)
+			expect(phrase).to eq("Today's Support Hero is #{user.name}")
+		end
+
+		it "should give you the holiday phrase if it's a California holiday" do 		
+			holiday_date = "1-1-2015".to_date
+			phrase = helper.get_header_phrase(holiday_date)
+			expect(phrase).to eq("It's a holiday! Go celebrate!")
+		end
+	end
 end
