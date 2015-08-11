385d39a63e4ca2f29299d2c4ac7a40878a273b38
Lukas Jones <joneslukas21@gmail.com>
2015-08-11 15:39:51 -0700

    removed day_assignments.js and put contents into days.js

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,3 +1,9 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function(){
+	$("html").on("mouseover", "td", function(){
+		$(this).find(".swap-form").show();
+	})
+
+	$("html").on("mouseout", "td", function(){
+		$(this).find(".swap-form").hide();
+	})
+})
