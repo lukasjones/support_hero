f22542df665fa3d10e9a06273315db7811748d7a
Lukas Jones <joneslukas21@gmail.com>
2015-08-10 21:15:01 -0700

    rails g controller scheduled months

---
+++ spec/controllers/scheduled_months_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ScheduledMonthsController, type: :controller do
+
+end
---
+++ spec/helpers/scheduled_months_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ScheduledMonthsHelper. For example:
+#
+# describe ScheduledMonthsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe ScheduledMonthsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
