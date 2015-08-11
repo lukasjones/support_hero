3da1e996e821605b579466e7d1eff0751ee03908
Lukas Jones <joneslukas21@gmail.com>
2015-08-10 21:13:20 -0700

    fixed user helper to work with Day class

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
 		elsif ["01/01", "01/19", "02/16", "03/31", "05/25", "07/04", "09/07", "11/11", "11/26", "11/27", "12/25"].include?(date.strftime("%m/%d"))
 			"It's a holiday! Go celebrate!"
 		else
-			user = DayAssignment.where(date: date)[0].user
+			user = Day.where(date: date)[0].user
 			"Today's Support Hero is #{user.name}"
 		end
 	end
