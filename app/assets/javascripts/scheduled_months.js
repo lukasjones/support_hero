6db363afdb0d3fcc21a85b130052f34a726633c9
Lukas Jones <joneslukas21@gmail.com>
2015-08-11 15:41:52 -0700

    removed calendar.js and put contents into scheduled_month.js

--- app/assets/javascripts/scheduled_months.js
+++ app/assets/javascripts/scheduled_months.js
@@ -24,6 +24,22 @@
 		.fail(function(){
 			console.log("didn't work");
 		})
+
+
+		$("html").on("click", ".cal_link", function(e){
+			e.preventDefault();
+			url = $(this).attr("href");
+			$.ajax({
+				url: url,
+				type: "get"
+			})
+			.success(function(response){
+				$(".table-container").html(response)
+			})
+			.fail(function(){
+				console.log("failed");
+			})
+		})
 	})
 	
 // })
