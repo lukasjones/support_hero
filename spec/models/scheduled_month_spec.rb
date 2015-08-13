d625e74bddd23accd79dbfd6853af6fe7bf5860d
Lukas Jones <joneslukas21@gmail.com>
2015-08-13 11:51:27 -0700

    wrote tests for scheduled month model

--- spec/models/scheduled_month_spec.rb
+++ spec/models/scheduled_month_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ScheduledMonth, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+	it "should save a scheduled month with a unique month and year" do
+		scheduled_month = ScheduledMonth.new(month: 1, year: 2000)
+		expect(scheduled_month.save).to eq(true)
+	end
+
+	it "should not save a duplicate scheduled month" do
+		ScheduledMonth.create(month: 8, year: 2014)
+		scheduled_month = ScheduledMonth.new(month: 8, year: 2014)
+		expect(scheduled_month.save).to eq(false)
+	end
+  
 end
