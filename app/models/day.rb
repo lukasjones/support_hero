25f1ebaf0a98647457242b536b7062b2dc3ac384
Lukas Jones <joneslukas21@gmail.com>
2015-08-14 11:39:13 -0700

    removed unneccessary comments

--- app/models/day.rb
+++ app/models/day.rb
@@ -53,39 +53,17 @@ class Day < ActiveRecord::Base
 		end
 	end
 
-
-	#         January 1, 2015  												January 5, 2015
-	# { date: '1-1-2015', user: @user, swap_request: '1-5-2015', has_requested_swap: false }
-
-
-	# day1
-		# date:                8-5-2015
-		# swap_request:        nil
-		# has_requested_swap:  true
-		
-	# day2
-		# date:                8-12-2015
-		# swap_request:        8-5-2015
-		# has_requested_swap:  false
-
-
-	
-
 	def cannot_request_multiple_swaps_for_same_date
 		return if !user
 		if self.swap_request
 			date_requesting_to_swap = self.swap_request
 			day_requesting = Day.where(date: date_requesting_to_swap)[0]
-			# byebug
 			if day_requesting.has_requested_swap_was != false 
 				errors.add(:cannot_request_same_day, "You have already asked to swap this day. ")
 			end
 		end
 
-		# has requested swap
 	end
-
-
 
 	def cannot_be_weekend
 		return if !user
@@ -123,30 +101,3 @@ class Day < ActiveRecord::Base
   
 
 end
-
-# class Day
-# 	include ActiveModel::Model
-
-# 	attr_reader :date, :week_day_name, :num, :scheduled_user_name, :scheduled_user_id
-
-# 	attr_accessor :scheduled_user
-
-
-# 	def initialize(attributes={})
-# 		@date                = attributes[:date] || nil
-# 		@scheduled_user      = attributes[:user] || nil 
-# 		@scheduled_user_name = @scheduled_user ? @scheduled_user.name : nil
-# 		@scheduled_user_id   = @scheduled_user ? @scheduled_user.id : nil
-# 		@week_day_name       = @date ? @date.strftime("%A") : nil
-# 		@num                 = @date ? @date.day : nil
-# 	end
-
-# 	def update_user(user)
-# 		@scheduled_user      = user
-# 		@scheduled_user_name = user.name
-# 		@scheduled_user_id   = user.id
-# 	end
-
-	
-
-# end
