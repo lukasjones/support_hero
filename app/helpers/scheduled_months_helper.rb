12c217e14ac983fc53bbd04ce912bddee2d49c7a
Lukas Jones <joneslukas21@gmail.com>
2015-08-14 18:07:08 -0700

    added some view helpers to simplify code

--- app/helpers/scheduled_months_helper.rb
+++ app/helpers/scheduled_months_helper.rb
@@ -9,5 +9,17 @@ module ScheduledMonthsHelper
 	def pretty_date(date)
 		"#{Date::MONTHNAMES[date.month]} #{date.day}, #{date.year}"
 	end
+
+	def ugly_date(current_day)
+		"#{current_day.day_of_month_num}-#{session[:month_num]}-#{session[:year]}"
+	end
+
+	def set_selected(current_day)
+		if session[:selected_user] == current_day.user_name
+			'class=selected' 
+		elsif current_day.date.month != session[:month_num]
+			'class=faded'
+		end
+	end
 	
 end
